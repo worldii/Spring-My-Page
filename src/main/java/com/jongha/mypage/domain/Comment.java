9efd0935d3ec335eb1361f890c0df65d4b214d7a
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-04-05 22:01:48 +0900

    Style : 코드 수정

--- src/main/java/com/jongha/mypage/domain/Comment.java
+++ src/main/java/com/jongha/mypage/domain/Comment.java
@@ -3,13 +3,7 @@ package com.jongha.mypage.domain;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -23,7 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	@Column(name="comment_id")
 	private Long id;
 
